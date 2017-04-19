953362c624644a4b483c4155f02ef0d6089acc10
L. David Baron <dbaron@dbaron.org>
2017-04-19 21:42:53 +0900

    Add a comment explaining what git-hash is for.

--- build.rs
+++ build.rs
@@ -5,6 +5,9 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    // Write the hash of our current revision to the file
+    // ${OUT_DIR}/git-hash, so that it can be tetxually included in the
+    // binary using include_str!() in src/main.rs.
     let output_dir = env::var("OUT_DIR").unwrap();
     let output_file_path = Path::new(&output_dir).join("git-hash");
     let mut output_file = File::create(&output_file_path).unwrap();
