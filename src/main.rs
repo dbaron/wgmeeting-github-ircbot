dbaron/wgmeeting-github-ircbot

commit ac2ad7e8ec43b43c6eee2abc850aa426ce71f4ff

Also respond (but differently) when there's no current topic set.

src/main.rs
@@ -382,7 +382,16 @@ impl<'opts> ChannelData<'opts> {
             self.end_topic(server);
         }
         match self.current_topic {
-            None => None,
+            None => {
+                match extract_github_url(&line.message, self.options, &None) {
+                    (Some(_), None) => Some(String::from("I can't set a github URL because you haven't started a topic.")),
+                    (None, Some(ref extract_response)) => {
+                        Some(String::from("I can't set a github URL because you haven't started a topic.  Also, ") +
+                             extract_response)
+                    }
+                    _ => panic!("unexpected state"),
+                }
+            }
             Some(ref mut data) => {
                 let (new_url_option, extract_failure_response) =
                     extract_github_url(&line.message, self.options, &data.github_url);

commit 004933c363657bf2954ac910a69e624c1e7057f4

Fix panic on a line with nothing special when no topic is set.
This was a regression from ac2ad7e8ec43b43c6eee2abc850aa426ce71f4ff.

src/main.rs
@@ -398,6 +398,7 @@ impl<'opts> ChannelData<'opts> {
                         Some(String::from("I can't set a github URL because you haven't started a topic.  Also, ") +
                              extract_response)
                     }
+                    (None, None) => None,
                     _ => panic!("unexpected state"),
                 }
             }