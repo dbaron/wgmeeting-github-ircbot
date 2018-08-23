33fa1dd05070ff8434e93c2b2c93330c603b1aa8
Xidorn Quan <me@upsuper.org>
2018-08-23 15:55:09 +1000

    Simplify reading configuration.

--- src/main.rs
+++ src/main.rs
@@ -7,57 +7,46 @@
 
 extern crate env_logger;
 extern crate irc;
-#[macro_use]
-extern crate lazy_static;
 extern crate tokio_core;
 extern crate wgmeeting_github_ircbot;
 
 use irc::client::prelude::{Client, ClientExt, Config, Future, IrcClient, Stream};
 use irc::client::PackedIrcClient;
-use std::collections::HashMap;
 use std::env;
-use std::ffi::OsString;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 use tokio_core::reactor::Core;
 use wgmeeting_github_ircbot::*;
 
+fn read_config() -> Config {
+    let mut args = env::args_os();
+    if args.len() != 3 {
+        eprintln!(
+            "syntax: {} <config file> <github access token file>\n",
+            env::args().next().unwrap()
+        );
+        ::std::process::exit(1);
+    }
+    let (_, config_file, token_file) = (
+        args.next().unwrap(),
+        args.next().unwrap(),
+        args.next().unwrap(),
+    );
+
+    let mut config = Config::load(config_file)
+        .expect("couldn't load configuration file");
+    let token = fs::read_to_string(token_file)
+        .expect("couldn't read github access token file");
+    let _ = config
+        .options
+        .as_mut()
+        .unwrap()
+        .insert("github_access_token".to_string(), token);
+    config
+}
+
 fn main() {
     env_logger::init();
-
-    lazy_static! {
-        static ref ARGS: Vec<OsString> = {
-            let args = env::args_os().skip(1).collect::<Vec<_>>();
-            if args.len() != 2 {
-                eprintln!("syntax: {} <config file> <github access token file>\n",
-                    env::args_os().nth(0)
-                        .map(|osstring| osstring.into_string().unwrap_or(String::from("")))
-                        .unwrap_or(String::from("")));
-                ::std::process::exit(1);
-            }
-            args
-        };
-        static ref CONFIG: Config = {
-            Config::load(ARGS[0].clone()).expect("couldn't load configuration file")
-        };
-        static ref GITHUB_ACCESS_TOKEN: String = {
-            let mut f = File::open(ARGS[1].clone())
-                .expect("github access token file (second argument) not found");
-            let mut token = String::new();
-            let _numbytes =
-                f.read_to_string(&mut token).expect("couldn't read github access token file");
-            token
-        };
-        static ref OPTIONS: HashMap<String, String> = {
-            let mut options =
-                CONFIG.options.as_ref().expect("No options property within configuration?").clone();
-            // Store the access token with the options, even though we read it from a different
-            // place.
-            let _oldval =
-                options.insert("github_access_token".to_string(), GITHUB_ACCESS_TOKEN.to_string());
-            options
-        };
-    }
+    let config: &'static Config = Box::leak(Box::new(read_config()));
 
     // FIXME: Add a way to ask the bot to reboot itself?
 
@@ -65,7 +54,7 @@ fn main() {
     let handle = core.handle();
     let mut irc_state = IRCState::new(GithubType::RealGithubConnection, &handle);
 
-    let irc_client_future = IrcClient::new_future(handle, &CONFIG).expect(
+    let irc_client_future = IrcClient::new_future(handle, config).expect(
         "Couldn't initialize server \
          with given configuration file",
     );
@@ -75,7 +64,8 @@ fn main() {
     irc.identify().unwrap();
 
     let ircstream = irc.stream().for_each(|message| {
-        process_irc_message(&irc, &mut irc_state, &OPTIONS, message);
+        let options = config.options.as_ref().unwrap();
+        process_irc_message(&irc, &mut irc_state, options, message);
         Ok(())
     });
 
