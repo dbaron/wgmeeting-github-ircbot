9783aab9b979b6925abc56aad5af3343b0f83348
L. David Baron <dbaron@dbaron.org>
2019-10-31 09:08:24 -0700

    Work correctly when another user on the same VM is using screen.

--- start-ircbot-if-needed.sh
+++ start-ircbot-if-needed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! ps -C screen > /dev/null
+if ! ps -C screen -o user | grep "^ircbot$" > /dev/null
 then
 	screen -d -m bash -l -c "(cd ~/wgmeeting-github-ircbot && git pull && RUST_BACKTRACE=1 RUST_LOG=wgmeeting_github_ircbot cargo run --release ./src/config.toml ./github_access_token_file) > ~/logs/ircbot.$(date +%F.%H%M%S).$$.log 2>&1"
 fi
