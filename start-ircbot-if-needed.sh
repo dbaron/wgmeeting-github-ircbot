a1b446caec5c7bc45483b0847e92ad889881ac93
L. David Baron <dbaron@dbaron.org>
2019-10-31 09:06:08 -0700

    Add script that I've been using to start/restart the bot.

---
+++ start-ircbot-if-needed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if ! ps -C screen > /dev/null
+then
+	screen -d -m bash -l -c "(cd ~/wgmeeting-github-ircbot && git pull && RUST_BACKTRACE=1 RUST_LOG=wgmeeting_github_ircbot cargo run --release ./src/config.json ./github_access_token_file) > ~/logs/ircbot.$(date +%F.%H%M%S).$$.log 2>&1"
+fi
