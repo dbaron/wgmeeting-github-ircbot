b4018acab0513049e7645e3b8db197b3bd521d80
L. David Baron <dbaron@dbaron.org>
2019-10-31 09:07:55 -0700

    Update for json->toml that I made along with #34.

--- start-ircbot-if-needed.sh
+++ start-ircbot-if-needed.sh
@@ -2,5 +2,5 @@
 
 if ! ps -C screen > /dev/null
 then
-	screen -d -m bash -l -c "(cd ~/wgmeeting-github-ircbot && git pull && RUST_BACKTRACE=1 RUST_LOG=wgmeeting_github_ircbot cargo run --release ./src/config.json ./github_access_token_file) > ~/logs/ircbot.$(date +%F.%H%M%S).$$.log 2>&1"
+	screen -d -m bash -l -c "(cd ~/wgmeeting-github-ircbot && git pull && RUST_BACKTRACE=1 RUST_LOG=wgmeeting_github_ircbot cargo run --release ./src/config.toml ./github_access_token_file) > ~/logs/ircbot.$(date +%F.%H%M%S).$$.log 2>&1"
 fi
