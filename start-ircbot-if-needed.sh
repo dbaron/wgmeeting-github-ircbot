76545523f8f73e53568b685f370ed157280caba8
L. David Baron <dbaron@dbaron.org>
2024-03-29 15:32:30 -0400

    Compile with -j1.

--- start-ircbot-if-needed.sh
+++ start-ircbot-if-needed.sh
@@ -2,5 +2,5 @@
 
 if ! ps -C screen -o user | grep "^ircbot$" > /dev/null
 then
-	screen -d -m bash -l -c "(cd ~/wgmeeting-github-ircbot && git pull && RUST_BACKTRACE=1 RUST_LOG=wgmeeting_github_ircbot cargo run --release ./src/config.toml ./github_access_token_file) > ~/logs/ircbot.$(date +%F.%H%M%S).$$.log 2>&1"
+	screen -d -m bash -l -c "(cd ~/wgmeeting-github-ircbot && git pull && RUST_BACKTRACE=1 RUST_LOG=wgmeeting_github_ircbot cargo run -j1 --release ./src/config.toml ./github_access_token_file) > ~/logs/ircbot.$(date +%F.%H%M%S).$$.log 2>&1"
 fi
